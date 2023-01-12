28dc4c9839b0588840ff81a5f3c7a46e5cd5ee1f
Michael Petrosovich <mchio@yandex.ru>
2023-01-13 00:31:27 +0300

    initial commit

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -3,17 +3,27 @@ import {FC} from "react";
 // @ts-ignore
 import s from "./Layout.module.scss";
 import {Header} from "./Inside/Header";
+import {SideBar} from "./Inside/SideBar";
 
 export const Layout: FC<any> = ({children}) => {
     return (
         <div className={s.bg1}>
-        <div className={s.bgIn}>
-            <div className={s.header}>header</div>
-            <div className={s.bg2}>
-                <div className={s.menu}>меню{children}</div>
-                <div className={s.kont}>контент{children}</div>
+
+            <div className={s.header}>
+                <div className={s.htext}>header</div>
             </div>
-        </div>
+
+            <div className={s.bg2}>
+
+                <div className={s.menu}>
+                    <div className={s.tmenu}><SideBar/></div>
+                </div>
+
+                <div className={s.kont}>
+                    <div className={s.tkont}>контент{children}</div>
+                </div>
+
+            </div>
         </div>
     )
 }
